160d15d48aae4c89173377f664b80c29bdb184b7
Varshitha H. R. <157672705+Varshitha1245@users.noreply.github.com>
2026-05-14 11:53:16 +0530

    Delete resume/Casestudy_onlineshopping directory

--- resume/Casestudy_onlineshopping/src/com/tnsif/onlineshopping/entities/Admin.java
+++
@@ -1,7 +0,0 @@
-package com.tnsif.onlineshopping.entities;
-
-public class Admin extends User {
-    public Admin(int userId, String username, String email, String password) {
-        super(userId, username, email, password);
-    }
-}
--- resume/Casestudy_onlineshopping/src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module Casestudy_onlineshopping {
-}
